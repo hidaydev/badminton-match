f6a116c5c8091aebae5da5491186b15d87376bf2
hidaydev <me.hidaydev@gmail.com>
2026-04-29 18:34:38 +0700

    fix: hide Close button on shared session page

--- src/pages/SharedSessionPage.tsx
+++ src/pages/SharedSessionPage.tsx
@@ -96,7 +96,6 @@ export default function SharedSessionPage() {
       gameScores={gameScores}
       onTogglePlayedGame={handleTogglePlayed}
       onSetGameScore={handleSetScore}
-      onClose={() => navigate('/')}
       title={snapshot.session.title ?? ''}
       date={snapshot.session.date ?? ''}
     />
