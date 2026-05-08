38c6db7a6621ea717c7fabc8983837e0b3a5c0f2
hidaydev <me.hidaydev@gmail.com>
2026-05-08 18:12:32 +0700

    fix: increase max player count to 32

--- src/pages/SetupPage.tsx
+++ src/pages/SetupPage.tsx
@@ -244,7 +244,7 @@ export default function SetupPage() {
             label="Players"
             value={session.playerCount}
             min={4}
-            max={20}
+            max={32}
             onChange={setPlayerCount}
             disabled={session.locked}
           />
