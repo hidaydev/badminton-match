2316d1411e30be822165da5aae6afdd4b5298573
hidaydev <me.hidaydev@gmail.com>
2026-05-14 10:38:25 +0700

    feat: add instagram post template config

---
+++ src/config/instagramTemplates.ts
@@ -0,0 +1,20 @@
+// src/config/instagramTemplates.ts
+
+export interface PostTemplate {
+  id: string
+  label: string
+  width: number
+  height: number
+  header?: string  // absolute URL path to PNG asset
+  footer?: string  // absolute URL path to PNG asset
+}
+
+export const instagramTemplates: PostTemplate[] = [
+  {
+    id: 'portrait-v1',
+    label: 'Portrait 4:5',
+    width: 1080,
+    height: 1350,
+    footer: '/instagram-footer.png',
+  },
+]
