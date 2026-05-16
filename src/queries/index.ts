37d1ce8630d14f2cd5daf8f25a0320d330f6647d
hidaydev <me.hidaydev@gmail.com>
2026-05-16 18:25:10 +0700

    fix: remove redundant useReplacePlayer re-export

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,6 +1,5 @@
 export * from './types'
 export * from './players'
 export * from './sessions'
-export { useReplacePlayer } from './sessions'
 export * from './tournament'
 export { TOURNAMENT_ID } from './endpoints'
